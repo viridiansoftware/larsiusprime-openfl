f0a5bf63008ba40eb25d439537b1101ec2571b69
James Gray <james@james4k.com>
2017-02-25 21:35:50 -0600

    cherrypick

--- openfl/display/Stage.hx
+++ openfl/display/Stage.hx
@@ -759,6 +759,7 @@ class Stage extends DisplayObjectContainer implements IModule {
 		#end
 		
 		__broadcast (new Event (Event.ENTER_FRAME), true);
+		__broadcast (new Event (Event.EXIT_FRAME), true);
 		
 		if (__invalidated) {
 			
